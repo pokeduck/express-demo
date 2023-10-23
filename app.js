d634adbcbe7e9c2872f95acb57d578740d96d43b
pokeduck <40366083+pokeduck@users.noreply.github.com>
2023-10-23 19:04:07 +0800

    remove unused code.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import express, { Router } from "express";
 
 import cookieParser from "cookie-parser";
-import { body, param, query, check } from "express-validator";
 import cors from "cors";
 import { corsOptions } from "./config/cors.config.js";
-import { NOTFOUND } from "dns";
 import * as ErrorHandler from "./middlewares/error-handler.js";
 
 import * as url from "url";
@@ -24,16 +22,15 @@ const logger = (req, res, next) => {
 app.disable("x-powered-by");
 app.set("views", __dirname + "/pages");
 app.set("view engine", "ejs");
+
 // parse requests of content-type - application/json
-app.use(express.json()); /* bodyParser.json() is deprecated */
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(
-  express.urlencoded({ extended: true })
-); /* bodyParser.urlencoded() is deprecated */
+app.use(express.urlencoded({ extended: true }));
 
 //app.use(logger);
 
